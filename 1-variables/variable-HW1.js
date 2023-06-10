dbae5a2a2f94a7fad4e53f8cd552207d82f2518f
John Doexioiri04 <mach-dillanger@mahmoudbdulsmbp.mynetworksettings.com>
2023-06-09 21:35:46 -0400

    completed variable HW1

---
+++ 1-variables/variable-HW1.js
@@ -0,0 +1,32 @@
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+let name= "benji"
+console.log (name)
+let species= "yorkie"
+console.log (species)
+let age= 6
+console.log (age)
+let skincolor= "brindle"
+console.log (skincolor)
+let inches= 9
+console.log (inches)
+let breed= "hyperallergenic"
+console.log (breed)
+
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+let carName= "lamborghini"
+console.log (carName)
+let color= "red"
+console.log (color)
+let horsepower= 657
+console.log (horsepower)
+let fueltank= 22.5
+console.log(fueltank)
+let founded= "Italy"
+console.log(founded)
+let inventor= "ferruccio Lamborghini"
+console.log(inventor)
