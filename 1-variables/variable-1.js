fae449b2121ef8aa61e1bdf662de64cda7c04ae2
John Doexioiri04 <mach-dillanger@mahmoudbdulsmbp.mynetworksettings.com>
2023-06-09 20:44:52 -0400

    completed variable1

---
+++ 1-variables/variable-1.js
@@ -0,0 +1,30 @@
+/*
+  Create a .js file that satisfies assignment below
+  For each exercise you'll want to log the result to the console to check your work
+*/
+
+
+// 1. Declare a new variable and set it equal to the string of your name
+let name = "Xiomara"
+console.log(typeof name)
+// 2. Declare a new variable and set it equal to a number 
+let number = 4
+console.log(typeof number)
+// 3. Set a variable to a boolean value 
+let boolean = true
+console.log(boolean)
+// 4. Create a variable called carName, assign the value Volvo to it.
+let carName = "Volvo"
+console.log(carName)
+// 5. Create a variable called num, assign the value 50 to it.
+let num = 50
+console.log(num)
+// 6. Display the sum of 5 + 10, using two variables: num1 and num2.
+let num1 = 5
+let num2 = 10
+console.log(num1 + num2)
+let sum = num1 + num2
+console.log(sum)
+// 7. Create a variable called num3, assign the addition of num1 and num2 to it.
+let num3 = num1 + num2
+console.log(sum)
